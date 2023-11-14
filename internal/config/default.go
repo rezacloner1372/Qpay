366524bb5a8c43cc52306aef7922a57cd14b2052
MohammadAli Karimi <ma.karimi76@gmail.com>
2023-11-14 17:53:16 +0400

    chore: add server config

--- internal/config/default.go
+++ internal/config/default.go
@@ -2,10 +2,15 @@ package config
 
 import (
 	"Qpay/internal/db"
+	"Qpay/internal/server"
 )
 
 func Default() *Config {
 	return &Config{
+		Server: &server.Config{
+			Host: "localhost",
+			Port: 8080,
+		},
 		DB: &db.Config{
 			Host:     "localhost",
 			Port:     3306,
