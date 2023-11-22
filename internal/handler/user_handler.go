d7202832d446f18b59caf8432a544a25ef2ff9de
MohammadAli Karimi <ma.karimi76@gmail.com>
2023-11-22 20:37:51 +0400

    merge register user on signup handler

--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -1,7 +1,10 @@
 package handler
 
 import (
+	"Qpay/internal/model"
 	"Qpay/internal/repository"
+	"Qpay/pkg/utils"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -25,7 +28,23 @@ func NewUserHandler(userRepository repository.UserRepository) userHandler {
 
 func (s *userHandler) Signup() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return c.String(http.StatusOK, "Signup")
+		email := c.FormValue("email")
+		password := c.FormValue("password")
+
+		hashedPassword, err := utils.HashPassword(password)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "Registration failed")
+		}
+
+		user := model.User{Email: email, Password: hashedPassword}
+		result, err := s.repository.Create(user)
+		fmt.Println(111, result, err)
+
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "Registration failed")
+		}
+
+		return c.JSON(http.StatusOK, result)
 	}
 }
 
