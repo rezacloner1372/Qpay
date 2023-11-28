3730f465a1789c4b8904be3e845115a7b61a31a9
Amir Mohseninia <mohseninia.11@gmail.com>
2023-11-28 19:10:07 +0000

    Update PaymentGateways Model and Table

--- internal/model/payment_gateways.go
+++ internal/model/payment_gateways.go
@@ -2,18 +2,18 @@ package model
 
 import (
 	"time"
-
-	"github.com/jinzhu/gorm"
 )
 
 type PaymentGateways struct {
-	gorm.Model
-	Name              string    `gorm:"not null"`
-	UserId            string    `gorm:"not null"`
-	PersonalizedUrl   string    `gorm:"not null"`
-	Tariff            int       `grom:"not null"`
-	Status            int       `grom:"not null"`
-	RegisterationDate time.Time `gorm:"default:CURRENT_TIMESTAMP()"`
-	User              User      `gorm:"references:id"`
-	Tariffs           Tariffs   `gorm:"references:id"`
+	ID              uint       `gorm:"primaryKey"`
+	CreatedAt       time.Time  `gorm:"autoCreateTime"`
+	UpdatedAt       time.Time  `gorm:"autoUpdateTime"`
+	DeletedAt       *time.Time `gorm:"index"`
+	Title           string     `gorm:"not null"`
+	UserID          uint       `gorm:"not null"`
+	User            User       `gorm:"foreignKey:UserID"`
+	PersonalizedURL string     `gorm:"not null"`
+	IsDefault       int        `gorm:"not null;default:1"`
+	TariffID        *uint      // Assuming tariffs are pointers (optional field)
+	Tariff          *Tariffs   `gorm:"foreignKey:TariffID"`
 }
